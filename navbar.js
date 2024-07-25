7c942bb2e6e29986162b2bf3226c52b36810c39b
Cambobug <cambobug@rogers.com>
2024-07-25 17:49:32 -0400

    Added projects page as well as a navbar loader for easier nav bar editing across pages

---
+++ navbar.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    document.getElementById('navbar-div').innerHTML = '<ul class="navbar">\
+        <a href="index.html">\
+        <li>Home</li></a>\
+        <a href="projects.html">\
+        <li>Projects</li>\
+        </a><a href="contact.html">\
+        <li>Contact</li></a></ul>'
+    ;
+};
